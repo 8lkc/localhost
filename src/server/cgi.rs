dfacf478cf59dbdf406cf9a623014e7d477c0004
fakeita <fatimakeite05@gmail.com>
2025-02-05 16:34:46 +0000

    cgi test

--- src/server/cgi.rs
+++ src/server/cgi.rs
@@ -41,12 +41,16 @@ impl CommonGatewayInterface {
         request: &Request,
         stream: &mut TcpStream,
     ) -> Result<(), String> {
-        if !std::path::Path::new(request.resource.path()).exists() {
+        let script_path = format!("public{}", request.resource.path());
+        if !std::path::Path::new(&script_path).exists() {
+            dbg!(request.resource.path());
             return Err("CommonGatewayInterface script not found".to_string());
         }
         let query_string = request.resource.path().split('?').nth(1).unwrap_or("");
-        let output = Command::new(cgi_script)
-            .arg(request.resource.path())
+        dbg!(cgi_script);
+        let interpreter = self.get_interpreter(&script_path)?;
+        let output = Command::new(interpreter)
+            .arg(&format!("public{}", request.resource.path()))
             .env("REQUEST_METHOD", request.method.to_string())
             .env("QUERY_STRING", query_string)
             .output()
@@ -65,5 +69,19 @@ impl CommonGatewayInterface {
         }
 
         Ok(())
+    }
+
+    fn get_interpreter(&self, script_path: &str) -> Result<&str, String> {
+        let extension = script_path
+            .split('.')
+            .last()
+            .ok_or_else(|| "No file extension found".to_string())?;
+
+        match extension {
+            "py" => Ok("python3"),
+            "php" => Ok("php"),
+            "pl" => Ok("perl"),
+            _ => Err(format!("Unsupported CGI script extension: {}", extension)),
+        }
     }
 }
