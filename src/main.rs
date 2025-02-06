dd37cbe1aef5cb7cd920a6b886b0cf2729bcf09a
8lkc <papebilalysow@gmail.com>
2025-02-06 22:12:41 +0000

    🛠 read config file to set up multiple servers

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod server;
 
 use server::Server;
-use server::config::ServerConfig;
+use server::config::*;
 
 fn main() -> std::io::Result<()> {
     // Configuration de base
-    let config = ServerConfig::new();
-    
-    // Création et démarrage du serveur
+    let configs = Servers::new();
+
+    // Ensure there is at least one configuration and select it.
+    let config = configs.servers.first().expect("No server configuration found").clone();
+
+    // Create and start the server using the single ServerConfig.
     let mut server = Server::new(config)?;
     println!("Server starting on 127.0.0.1:8080");
     server.start()
--- src/server/config.rs
+++ src/server/config.rs
@@ -1,22 +1,39 @@
-use serde::Deserialize;
+use serde::{Deserialize};
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Servers {
+    pub servers: Vec<ServerConfig>,
+}
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct ServerConfig {
+    pub name: String,
     pub host: String,
     pub ports: Vec<u16>,
 }
 
-impl ServerConfig {
+impl Servers {
     pub fn new() -> Self {
         Self::read_config()
     }
 
     fn read_config() -> Self {
-        // let config = std::fs::read_to_string("config.toml").unwrap();
         let config = std::fs::read_to_string("config.yaml").unwrap();
-        // let checker = serde_yaml::from_str::<ServerConfig>(&config).expect("ERROR");
-        serde_yaml::from_str::<ServerConfig>(&config).expect("ERROR")
-        // println!("{:?}", checker);
-        // checker
+        serde_yaml::from_str::<Servers>(&config).expect("ERROR")
     }
 }
+
+// impl ServerConfig {
+//     pub fn new() -> Self {
+//         Self::read_config()
+//     }
+
+//     fn read_config() -> Self {
+//         // let config = std::fs::read_to_string("config.toml").unwrap();
+//         let config = std::fs::read_to_string("config.yaml").unwrap();
+//         // let checker = serde_yaml::from_str::<ServerConfig>(&config).expect("ERROR");
+//         serde_yaml::from_str::<ServerConfig>(&config).expect("ERROR")
+//         // println!("{:?}", checker);
+//         // checker
+//     }
+// }
