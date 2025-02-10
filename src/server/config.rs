6a87c5c57740229ebddc10e72dabdc63edfb5b96
8lkc <papebilalysow@gmail.com>
2025-02-10 21:10:06 +0000

    🛠️ refont the server side

--- src/server/config.rs
+++ src/server/config.rs
@@ -1,39 +1,21 @@
-use serde::{Deserialize};
+use serde::Deserialize;
 
 #[derive(Debug, Clone, Deserialize)]
-pub struct Servers {
+pub struct Config {
     pub servers: Vec<ServerConfig>,
 }
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct ServerConfig {
-    pub name: String,
     pub host: String,
     pub ports: Vec<u16>,
 }
 
-impl Servers {
-    pub fn new() -> Self {
-        Self::read_config()
-    }
+impl Config {
+    pub fn new() -> Self {Self::read_config()}
 
     fn read_config() -> Self {
         let config = std::fs::read_to_string("config.yaml").unwrap();
-        serde_yaml::from_str::<Servers>(&config).expect("ERROR")
+        serde_yaml::from_str::<Config>(&config).expect("ERROR")
     }
 }
-
-// impl ServerConfig {
-//     pub fn new() -> Self {
-//         Self::read_config()
-//     }
-
-//     fn read_config() -> Self {
-//         // let config = std::fs::read_to_string("config.toml").unwrap();
-//         let config = std::fs::read_to_string("config.yaml").unwrap();
-//         // let checker = serde_yaml::from_str::<ServerConfig>(&config).expect("ERROR");
-//         serde_yaml::from_str::<ServerConfig>(&config).expect("ERROR")
-//         // println!("{:?}", checker);
-//         // checker
-//     }
-// }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,62 +1,96 @@
 pub mod config;
 
-use std::io;
-use std::net::TcpListener;
-use self::config::ServerConfig;
+use std::{collections::HashMap, io::{Read, Write}, os::fd::AsRawFd};
+use libc::{epoll_ctl, epoll_event, epoll_wait, EPOLL_CTL_ADD, EPOLLIN, EPOLLOUT};
+use mio::net::{TcpListener, TcpStream};
 
-pub struct Server {
-    config: ServerConfig,
-    listener: Option<TcpListener>,
+use self::config::Config;
+
+pub struct Localhost {
+    pub config: Config,
 }
 
-impl Server {
-    pub fn new(config: ServerConfig) -> io::Result<Self> {
-        Ok(Server {
-            config,
-            listener: None,
-        })
-    }
+impl Localhost {
+    pub fn new() -> Self {Localhost {config: Config::new()}}
 
-    pub fn start(&mut self) -> io::Result<()> {
-        let addr = format!("{}:{}", self.config.host, self.config.ports[0]);
-        let listener = TcpListener::bind(&addr)?;
-        
-        println!("Listening on {}", addr);
+    pub fn start(&self) {
+        let epoll_fd = unsafe {libc::epoll_create1(0)};
+        if epoll_fd < 0 {panic!("Error creating epoll descriptor")}
 
-        self.listener = Some(listener);
+        if self.config.servers.is_empty() {panic!("No server configuration found")}
 
-        // Boucle principale simple
-        loop {
-            if let Some(listener) = &self.listener {
-                match listener.accept() {
-                    Ok((stream, addr)) => {
-                        println!("New connection from: {}", addr);
-                        self.handle_connection(stream)?;
-                    }
-                    Err(e) => {
-                        eprintln!("Error accepting connection: {}", e);
-                    }
+        let addresses = self.config.servers.iter().map(|config| {
+            config.ports.iter().map(|port| { format!("{}:{}", config.host, port) }).collect::<Vec<String>>()
+        }).flatten().collect::<Vec<String>>();
+        let mut servers = HashMap::new();
+        let mut clients = HashMap::new();
+
+        for address in &addresses {
+            let std_listener = std::net::TcpListener::bind(address).expect("Unable to start server");
+            std_listener.set_nonblocking(true).expect("Cannot set non-blocking");
+            let listener = TcpListener::from_std(std_listener);
+    
+            let fd = listener.as_raw_fd();
+            let mut event = libc::epoll_event {
+                events: EPOLLIN as u32,
+                u64: fd as u64,
+            };
+    
+            unsafe {
+                if epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &mut event) < 0 {
+                    panic!("Error adding file descriptor to epoll");
                 }
+            }
+            servers.insert(fd, listener);
+        }
+        println!("Servers listening on {:#?}", addresses);
+
+        let mut events: [epoll_event; 1024] = unsafe { std::mem::zeroed() };
+        loop {
+            let num_events = unsafe { epoll_wait(epoll_fd, events.as_mut_ptr(), events.len() as i32, -1) };
+            if num_events < 0 {panic!("ERROR: epoll_wait")}
+
+            for i in 0..num_events as usize {
+                let fd = events[i].u64 as i32;
+
+                if let Some(listener) = servers.get(&fd) {
+                    match listener.accept() {
+                        Ok((stream, address)) => {
+                            println!("New connection from: {:?}", address);
+                            let stream_fd = stream.as_raw_fd();
+                            clients.insert(stream_fd, stream);
+
+                            let mut event = libc::epoll_event {
+                                events: (EPOLLIN | EPOLLOUT) as u32,
+                                u64: stream_fd as u64,
+                            };
+
+                            unsafe {
+                                if epoll_ctl(epoll_fd, EPOLL_CTL_ADD, stream_fd, &mut event) < 0 {
+                                    panic!("Error adding client file descriptor to epoll");
+                                }
+                            }
+                        }
+                        Err(ref error) if error.kind() == std::io::ErrorKind::WouldBlock => {continue}
+                        Err(error) => {eprintln!("Error accepting connection: {:?}", error)}
+                    }
+                } else if let Some(stream) = clients.get_mut(&fd) {Self::handle_client(&mut *stream)}
             }
         }
     }
 
-    fn handle_connection(&self, mut stream: std::net::TcpStream) -> io::Result<()> {
-        use std::io::{Read, Write};
-        
-        // Buffer pour lire la requête
+    fn handle_client(stream: &mut TcpStream) {
         let mut buffer = [0; 1024];
-        stream.read(&mut buffer)?;
-
-        // Réponse HTTP basique
-        let response = "HTTP/1.1 200 OK\r\n\
-                       Content-Length: 13\r\n\
-                       \r\n\
-                       Hello, World!";
-
-        stream.write(response.as_bytes())?;
-        stream.flush()?;
-
-        Ok(())
+        match stream.read(&mut buffer) {
+            Ok(_) => {}
+            Err(ref error) if error.kind() == std::io::ErrorKind::WouldBlock => {return}
+            Err(error) => {
+                eprintln!("Error reading from stream: {:?}", error);
+                return;
+            }
+        }
+        let response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, world!";
+        stream.write_all(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
     }
 }
