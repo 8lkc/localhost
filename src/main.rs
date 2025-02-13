c53a49521d9b337be271ac726a4976dac8c9c0a4
8lkc <papebilalysow@gmail.com>
2025-02-13 15:59:53 +0000

    🚀 let's go❕

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,2 @@
-// use server::Localhost;
-
-mod server;
-
 fn main() {
-    // let localhost = Localhost::new();
-    // localhost.start();
 }
-
-// use std::fs::File;
-// use std::io::{self, BufRead, BufReader};
-
-// fn main() -> io::Result<()> {
-    
-//     // Open the file in read-only mode.
-//     let file = File::open("assets/header.txt")?;
-    
-//     // Wrap the file in a BufReader to handle buffering.
-//     let reader = BufReader::new(file);
-
-//     // Iterate over each line in the file.
-//     for line_result in reader.lines() {
-//         // Each line is returned as a Result<String, std::io::Error>.
-//         let line = line_result?;
-//         println!("   {}", line);
-//     }
-    
-//     Ok(())
-// }
